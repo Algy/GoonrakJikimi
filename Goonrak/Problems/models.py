c8a4788d9e3d0f4b175ce9f90c695f02885fa083
GaeNari <gaenari31@gmail.com>
2013-05-22 17:52:41 +0900

    making model

--- Goonrak/Problems/models.py
+++ Goonrak/Problems/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Problem(models.Model):
+	name = models.CharField(max_length=32)
+	score = models.IntegerField()
+	description = models.TextField()
